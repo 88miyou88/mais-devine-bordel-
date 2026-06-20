c54e89603d65317adc2dbdb451aaa13bd02ba615
88miyou88 <88miyou88@gmail.com>
2026-06-20 22:12:11 +0200

    Delete mais-devine-bordel-v0.9.7-complet directory

--- mais-devine-bordel-v0.9.7-complet/src/features/drawing/paper-mode.js
+++
@@ -1,9 +0,0 @@
-import { el } from "../../core/dom.js";
-
-export function displayDrawingSupport(support) {
-  el.drawCanvasArea.classList.toggle("hidden", support !== "phone");
-  el.drawPaperArea.classList.toggle("hidden", support !== "paper");
-  el.drawColorChoices.classList.toggle("hidden", support !== "phone");
-  el.drawToolsPanel.classList.toggle("hidden", support !== "phone");
-  el.drawPlayScreen.classList.toggle("draw-paper-mode", support === "paper");
-}
